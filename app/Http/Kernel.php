ebd8fc1f1632311e43e2acf046f7a8c3aab1a394
tomosu20 <tomosu.cre820@gmail.com>
2024-05-30 16:15:17 +0900

    add laravel-permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -66,5 +66,10 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        //laravel-permission
+        'role' => \Spatie\Permission\Middleware\RoleMiddleWare::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'roleOrPermission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
     ];
 }
